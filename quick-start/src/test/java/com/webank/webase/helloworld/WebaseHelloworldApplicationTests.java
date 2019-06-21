6aa78842ffec30dbbe3817be398ed1aeb19079a0
dwusiq <dwusiq@163.com>
2019-06-21 16:05:03 +0800

    1、 add: quick-start project

---
+++ quick-start/src/test/java/com/webank/webase/helloworld/WebaseHelloworldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webank.webase.helloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebaseHelloworldApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
