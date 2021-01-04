a1ddef2bf8e8f6d93ea4f1c0191df359d9ad1100
CodingCattwo <847701726@qq.com>
2021-01-04 16:37:42 +0800

    fix param

--- deploy/comm/check.py
+++ deploy/comm/check.py
@@ -329,7 +329,7 @@ def checkMemAndCpu():
     # cpu
     fisco_count_str = getCommProperties("node.counts")
     fisco_count = 2
-    if (fisco_ver_str != 'nodeCounts'):
+    if (fisco_count_str != 'nodeCounts'):
         fisco_count = int(fisco_count_str)
     # check 2 nodes, 4 nodes, more nodes memory free rate/cpu require
     flag=False
